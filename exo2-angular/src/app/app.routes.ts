44c14b3af5245df6e995e9e273a81b81b2f3d4d1
Sara Kjallouch <skjallouch@yahoo.com>
2024-12-10 16:45:29 +0100

    Modification section

--- exo2-angular/src/app/app.routes.ts
+++ exo2-angular/src/app/app.routes.ts
@@ -1,3 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+
+// Import your other standalone components if needed
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+import { GestionComponent } from './gestion/gestion.component';
+import { ContactComponent } from './contact/contact.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path: 'list', component: ListComponent },
+      { path: 'gestion', component: GestionComponent }
+    ]),
+    AppComponent // Import the standalone AppComponent
+  ]
+})
+export class AppModule { }
+
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'list', component: ListComponent },
+  { path: 'gestion', component: GestionComponent },
+  { path: 'contact', component: ContactComponent },
+];
+
+
--- exo2-angular/src/main.ts
+++ exo2-angular/src/main.ts
@@ -1,6 +1,23 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+/*import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+
 bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+  .catch((err) => console.error(err));*/
+
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { importProvidersFrom } from '@angular/core';
+import { HeaderComponent } from './app/header/header.component';
+import { FooterComponent } from './app/footer/footer.component';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(HeaderComponent, FooterComponent) // Import standalone components
+  ]
+}).catch(err => console.error(err));
+
